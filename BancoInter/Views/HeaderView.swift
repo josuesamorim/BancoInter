89d72bb8ab9cf452cad90f0810f62890b7cf7869
Josue Amorim <josuedesouzaamorim@gmail.com>
2023-05-26 11:32:35 -0300

    Used more descriptive names for variables and states. Separation of logic into different sections. In separate views or helper functions. Added explanatory comments can help you understand the intent and logic behind different parts of the visualization.

--- BancoInter/Views/HeaderView.swift
+++ BancoInter/Views/HeaderView.swift
@@ -9,104 +9,115 @@ import SwiftUI
 
 struct HeaderView: View {
     
-    @State private var isSearching: Bool = false
     @ObservedObject private var viewModel = ViewModel()
-    private let shape = Design().shape
+    @State private var showSearch: Bool = false
+    @State private var showSettings: Bool = false
+    @Binding var isBRSelected: Bool
+    @Binding var isUSSelected: Bool
+    
+    private let designShape = Design().shape
     
     var body: some View {
         HStack(alignment: .center) {
             
-            //MARK: - Logo
+            // MARK: - Logo
             Image("logo_inter")
                 .resizable()
                 .frame(width: 84, height: 25)
             
-            //MARK: - Account Selector
+            // MARK: - Account Selector
             accountSelector()
             
             Spacer()
             
-            //MARK: - Search
+            // MARK: - Search and Settings
             HStack {
-                Button {
-                    isSearching.toggle()
-                } label: {
-                    Image(systemName: "magnifyingglass")
-                }
-                .sheet(isPresented: $isSearching) {
-                    NavigationView {
-                        SearchView()
-                    }
-                }
-                
-                //MARK: - Settings
-                Image(systemName: "ellipsis")
-                    .rotationEffect(.degrees(-90))
+                searchButton
+                settingsButton
             }
             .font(.system(size: 20))
             .foregroundColor(.orange)
         }
         .frame(maxWidth: .infinity, alignment: .leading)
         .padding()
-        
     }
-   
-    //MARK: - Account Selectector Func
-    func accountSelector() ->  AnyView{
-        
-        let shape = Design().shape
-        
-        return AnyView (
-            
+    
+    // MARK: - Account Selector
+    func accountSelector() -> AnyView {
+        return AnyView(
             ZStack(alignment: .center) {
-                
                 RoundedRectangle(cornerRadius: 20)
                     .stroke(Color("inter_gray"), lineWidth: 1)
                     .frame(width: 100, height: 25, alignment: .center)
                 
-                HStack(alignment: .center) {
-                    ZStack {
-                        if viewModel.isBRSelected {
-                            shape.fill().foregroundColor(Color("flag_selected"))
+                HStack(alignment: .center, spacing: 0) {
+                    accountButton(imageName: "br_flag", isSelected: isBRSelected) {
+                        if !isBRSelected {
+                            isBRSelected.toggle()
                         }
-                        Image("br_flag")
-                            .resizable()
-                            .frame(width: 20, height: 15)
-                            .onTapGesture {
-                                if !viewModel.isBRSelected {
-                                    viewModel.isBRSelected.toggle()
-                                }
-                                viewModel.isUSSelected = false
-                            }
+                        isUSSelected = false
                     }
-                    .frame(width: 40, height: 20)
-                    
-                    ZStack {
-                        if viewModel.isUSSelected {
-                            shape.fill().foregroundColor(Color("flag_selected"))
+                    accountButton(imageName: "us_flag", isSelected: isUSSelected) {
+                        if !isUSSelected {
+                            isUSSelected.toggle()
                         }
-                        Image("us_flag")
-                            .resizable()
-                            .frame(width: 20, height: 15)
-                            .onTapGesture {
-                                if !viewModel.isUSSelected {
-                                    viewModel.isUSSelected.toggle()
-                                }
-                                viewModel.isBRSelected = false
-                            }
+                        isBRSelected = false
                     }
-                    .frame(width: 40, height: 20)
                 }
                 .frame(width: 70)
             }
         )
     }
     
+    func accountButton(imageName: String, isSelected: Bool, action: @escaping () -> Void) -> some View {
+        Button(action: action) {
+            ZStack {
+                if isSelected {
+                    designShape.fill().foregroundColor(Color("flag_selected"))
+                }
+                Image(imageName)
+                    .resizable()
+                    .frame(width: 20, height: 15)
+            }
+            .frame(width: 40, height: 20)
+        }
+    }
+    
+    // MARK: - Search Button
+    private var searchButton: some View {
+        Button(action: {
+            showSearch.toggle()
+        }) {
+            Image(systemName: "magnifyingglass")
+        }
+        .sheet(isPresented: $showSearch) {
+            NavigationView {
+                SearchView()
+            }
+        }
+    }
+    
+    // MARK: - Settings Button
+    private var settingsButton: some View {
+        Button(action: {
+            showSettings.toggle()
+        }) {
+            Image(systemName: "ellipsis")
+                .rotationEffect(.degrees(-90))
+        }
+        .sheet(isPresented: $showSettings) {
+            if #available(iOS 16, *) {
+                SettingsListView()
+                    .presentationDetents([.medium, .large])
+            } else {
+                SettingsListView()
+            }
+        }
+    }
 }
 
-
-struct Header_Previews: PreviewProvider {
+struct HeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        HeaderView()
+        HeaderView(isBRSelected: .constant(true), isUSSelected: .constant(false))
     }
 }
