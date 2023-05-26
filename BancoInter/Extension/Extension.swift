18c27b8e3cfbcdc337a0583ef803a364cce47d25
Josue Amorim <josuedesouzaamorim@gmail.com>
2023-05-26 11:30:41 -0300

    Added two new colors: "flagSelected, interBorder".

--- BancoInter/Extension/Extension.swift
+++ BancoInter/Extension/Extension.swift
@@ -10,4 +10,6 @@ import SwiftUI
 extension Color {
     static let interBackground = Color("inter_background")
     static let interBold = Color("inter_bold")
+    static let flagSelected = Color("flag_selected")
+    static let interBorder = Color("inter_gray")
 }
